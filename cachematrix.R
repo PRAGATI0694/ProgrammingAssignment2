0b2f5758b47c0d9c4b5485b5fe4cbf949b021d9b
PRAGATI0694 <pragatimisra0694@gmail.com>
2015-06-20 20:27:07 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeVector contains 4 functions: set, get, setsolve, getsolve
+## get:returns the matrix x stored in the main function
+## set:changes the matrix stored in the main function
 
-## Write a short comment describing this function
+## setmean and getmean:store the value of the input in a variable m 
+## into the main function makeVector (setmean) 
+## and return it (getmean)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+         }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+##  verify the value m, stored previously with getsolve, exists
+##  and is not NULL
+##  If it exists in memory, returns a message and the value m
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
